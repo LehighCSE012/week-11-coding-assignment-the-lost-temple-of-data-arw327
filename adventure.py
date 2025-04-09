32c13884965b387c32af29a3b5b63ce8a6e38b8b
arw327 <arw327@lehigh.edu>
2025-04-08 20:24:02 -0400

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,1 +1,6 @@
-# Your code goes here
+''' Adelaide's code practicing pandas'''
+#goal is to read in and extract information, journal dates, secret codes
+#template available, add one or two lines to each method
+#pylint will not like except statements, he removed all of them
+#to read in locations part, read.csv()
+
